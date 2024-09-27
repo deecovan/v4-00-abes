19e668db872b8fb06dfe58b5b1b0a2a6de8031f9
Dee <dee.covan@gmail.com>
2024-09-27 19:47:12 +0300

    Tested Working Button 1

--- Addings/Control/Button.gd
+++ Addings/Control/Button.gd
@@ -6,13 +6,38 @@ extends TextureButton
 @onready var time := $Time
 @onready var timer := $Timer
 
+var skill = null
+
 var change_key = "":
 	set(value):
 		change_key = value
 		key.text = value
-
 		shortcut = Shortcut.new()
 		var input_key := InputEventKey.new()
 		input_key.keycode = value.unicode_at(0)
 		
 		shortcut.events = [input_key]
+
+
+func _ready() -> void:
+	change_key = "1"
+	cooldown.max_value = timer.wait_time
+	set_process(false)
+	
+	
+func _process(_delta: float) -> void:
+	time.text = "%3.1f" % timer.time_left
+	cooldown.value = timer.time_left
+
+
+func _on_pressed() -> void:
+	timer.start()
+	disabled = true
+	set_process(true)
+
+
+func _on_timer_timeout() -> void:
+	time.text = ""
+	cooldown.value = 0
+	disabled = false
+	set_process(false)
