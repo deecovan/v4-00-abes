8bc800269a0905664241ca9df4bdb9bb6d85ace9
Dee <dee.covan@gmail.com>
2024-09-27 21:05:57 +0300

    Need to deploy start of the wideo: target.multi_shot() and

--- Addings/Control/FireShot.gd
+++ Addings/Control/FireShot.gd
@@ -4,3 +4,11 @@ class_name FireShot
 func _init(target) -> void:
 	cooldown = 1.0
 	animation_name = "Fire"
+	texture = preload("res://Addings/Control/Assets/FX/Skill Icons/48x48/skill_icons4.png")
+
+	super._init(target)
+	
+	
+func cast_spell(target) -> void:
+	super.cast_spell(target)
+	target.multi_shot()
