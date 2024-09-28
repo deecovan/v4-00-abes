1f9bfa08ac95a676437382ea74627606617c93f5
Dee <dee.covan@gmail.com>
2024-09-29 01:59:28 +0300

    Finally tested Skilbar

--- Addings/Control/SkillBar.gd
+++ Addings/Control/SkillBar.gd
@@ -1,6 +1,7 @@
 extends HBoxContainer
 
-var slots: Array
+var slots: Array 
+var skills: Array = [FireShot, WaterBall, Tornado, DarkBall, Ultimate]
 
 
 func _ready() -> void:
@@ -8,10 +9,4 @@ func _ready() -> void:
 	for i in slots.size():
 		pass
 		slots[i].change_key = str(i + 1)
-		
-		## Testing slots
-		slots[0].skill = FireShot.new(slots[0])
-		slots[1].skill = WaterBall.new(slots[1])
-		slots[2].skill = Tornado.new(slots[2])
-		slots[3].skill = DarkBall.new(slots[3])
-		slots[4].skill = Ultimate.new(slots[4])
+		slots[i].skill = skills[i].new(slots[i])
