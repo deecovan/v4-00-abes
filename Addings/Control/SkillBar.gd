1087b8c9cee34975559601f147c7732ea77a2b79
Dee <dee.covan@gmail.com>
2024-09-28 23:39:12 +0300

    It works until target.multi_shot() dont have the shot realisation

--- Addings/Control/SkillBar.gd
+++ Addings/Control/SkillBar.gd
@@ -5,5 +5,9 @@ var slots: Array
 
 func _ready() -> void:
 	slots = get_children()
-	for i in slots:
+	for i in slots.size():
+		pass
 		slots[i].change_key = str(i + 1)
+		
+		## Testing slots
+		slots[0].skill = FireShot.new(slots[0])
